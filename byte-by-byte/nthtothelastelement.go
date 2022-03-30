91cb3a728a7985f62427b5d54453375c89d11c85
Muthu Jattennavar <mjattenn@cisco.com>
2022-03-30 21:15:42 +0530

    42. Nth to the Last Element

---
+++ byte-by-byte/nthtothelastelement.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Gadigeppa-J/ds-algo-golang-nw/utils"
+)
+
+/*
+42. Nth to the Last Element
+Question:​ Given a linked list, and an input n, write a function that returns the nth-to-last element of the linked list.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+func main() {
+	fmt.Println(nthToLast(utils.CreateLinkedList(1), 1))
+}
+
+func nthToLast(l *utils.ListNode, index int) int {
+
+	p1 := l
+	p2 := l
+
+	for i := 0; i < index; i++ {
+		p2 = p2.Next
+		if p2 == nil {
+			panic("input Index out of bound")
+		}
+	}
+
+	for p2.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+
+	return p1.Val
+}
