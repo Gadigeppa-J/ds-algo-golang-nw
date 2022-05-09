85465e50b8c2d0d02786ab03f401bb5f748a0106
Muthu Jattennavar <mjattenn@cisco.com>
2022-05-09 13:23:18 +0530

    Balanced Parentheses

---
+++ amazon/balancedparentheses.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Gadigeppa-J/ds-algo-golang-nw/utils"
+)
+
+func main() {
+	fmt.Println(isValid("(("))
+}
+
+func isValid(s string) bool {
+
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	stack := utils.NewStack()
+
+	for _, c := range s {
+
+		if string(c) == "(" || string(c) == "[" || string(c) == "{" {
+			stack.Push(int(c))
+		} else if string(c) == ")" {
+			if stack.IsEmpty() {
+				return false
+			}
+
+			c := string(rune(stack.Pop()))
+
+			if c != "(" {
+				return false
+			}
+
+		} else if string(c) == "]" {
+			if stack.IsEmpty() {
+				return false
+			}
+
+			c := string(rune(stack.Pop()))
+
+			if c != "[" {
+				return false
+			}
+
+		} else if string(c) == "}" {
+			if stack.IsEmpty() {
+				return false
+			}
+
+			c := string(rune(stack.Pop()))
+
+			if c != "{" {
+				return false
+			}
+		}
+
+	}
+
+	return stack.IsEmpty()
+}
