5cb23e54b24e339b062d2a505dd06e4f0f8f6248
Muthu Jattennavar <mjattenn@cisco.com>
2022-05-21 19:50:38 +0530

    String Compression

---
+++ byte-by-byte/stringcompression.go
@@ -0,0 +1,54 @@
+package main
+
+/*
+https://leetcode.com/problems/string-compression/
+
+TC: O(n)
+TC: O(1)
+*/
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	chars := []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}
+	fmt.Println(compress(chars))
+
+}
+
+func compress(chars []byte) int {
+
+	i, j := 0, 0
+	index := 0
+
+	for j < len(chars) {
+
+		i = j
+		c := chars[i]
+
+		for j < len(chars) && chars[i] == chars[j] {
+			j++
+		}
+
+		chars[index] = c
+		index++
+
+		d := j - i
+		if d > 1 {
+
+			ns := strconv.FormatInt(int64(d), 10)
+
+			for _, dc := range ns {
+				chars[index] = byte(dc)
+				index++
+			}
+		}
+
+	}
+
+	return index
+
+}
