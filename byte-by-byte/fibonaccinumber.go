a20725bc4942c731697fb42af18984c98cba0f75
Muthu Jattennavar <mjattenn@cisco.com>
2022-05-21 20:16:36 +0530

    Fibonacci Number

--- byte-by-byte/fibonaccinumber.go
+++ byte-by-byte/fibonaccinumber.go
@@ -31,3 +31,20 @@ func fib(n int) int {
 	return j
 
 }
+
+/*
+// recursive
+func fib(n int) int {
+
+    if n == 0{
+        return 0
+    }
+
+    if n == 1{
+        return 1
+    }
+
+    return fib(n-1) + fib(n-2)
+
+}
+*/
