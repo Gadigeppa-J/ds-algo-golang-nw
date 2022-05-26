a46a7528b44b56ff491544a984ec177b6aec193d
Gadigeppa-J <gadigeppa.code@gmail.com>
2022-05-26 17:33:23 +0530

    Longest Increasing Subsequence

---
+++ amazon/longestincreasingsubsequence.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+https://leetcode.com/problems/longest-increasing-subsequence/
+TC: O(n^2)
+SC: O(n)
+
+Can we solve it in O(n log(n))??
+*/
+
+import (
+	"fmt"
+
+	"github.com/Gadigeppa-J/ds-algo-golang-nw/utils"
+)
+
+func main() {
+
+	nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
+	fmt.Println(lengthOfLIS(nums))
+
+}
+
+func lengthOfLIS(nums []int) int {
+
+	longArr := make([]int, len(nums))
+
+	for i := range longArr {
+		longArr[i] = 1
+	}
+
+	for j := 0; j < len(nums); j++ {
+		for i := 0; i < j; i++ {
+			if nums[i] < nums[j] {
+				v := longArr[i] + 1
+				if v > longArr[j] {
+					longArr[j] = v
+				}
+			}
+		}
+	}
+
+	longest := 0
+
+	for _, v := range longArr {
+		longest = utils.Max(longest, v)
+	}
+
+	return longest
+}
