3bec8d5ee2d4310da423122eb53b429f1398f128
Muthu Jattennavar <mjattenn@cisco.com>
2022-05-30 13:33:37 +0530

    Longest Palindromic Substring

---
+++ amazon/longestpalindromicsubstring.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestPalindrome("cbbd"))
+}
+
+func longestPalindrome(s string) string {
+
+	resStr := ""
+	resLen := 0
+
+	sl := len(s)
+
+	for i, _ := range s {
+
+		// even palindrome
+		r, l := i, i
+
+		for l >= 0 && r < sl && s[l] == s[r] {
+			if r-l+1 > resLen {
+				resLen = r - l + 1
+				resStr = s[l : r+1]
+			}
+			l--
+			r++
+		}
+
+		// odd palindrome
+		r, l = i+1, i
+
+		for l >= 0 && r < sl && s[l] == s[r] {
+			if r-l+1 > resLen {
+				resLen = r - l + 1
+				resStr = s[l : r+1]
+			}
+			l--
+			r++
+		}
+
+	}
+
+	return resStr
+}
